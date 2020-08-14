af8c252922000898a182ff05da5d447411bcddd1
smg-73 <sixtomg7@gmail.com>
2020-08-14 15:45:13 -0400

    submits exercise 2

---
+++ script.js
@@ -0,0 +1,5 @@
+// 1 - Select the section with an id of container without using querySelector.
+
+document.getElementById("container");
+
+// 2 - Select the section with an id of container using querySelector.
