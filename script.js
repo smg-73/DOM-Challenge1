e11446293de3062599db1bb423fc4ca45b33b1c5
smg-73 <sixtomg7@gmail.com>
2020-08-15 19:15:59 -0400

    Submits exercise 3

--- script.js
+++ script.js
@@ -5,3 +5,5 @@ document.getElementById("container");
 // 2 - Select the section with an id of container using querySelector.
 
 document.querySelector("#container");
+
+// 3 - Select all of the list items with a class of "second".
